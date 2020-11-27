54752ea9ddfb20a0010f7647e09df47bbadb0e32
Jose Manuel <jmdcarq89@gmail.com>
2020-11-27 22:28:39 +0100

    Test-1_comment

--- 03-counter-app/src/tests/base/09-promesas.test.js
+++ 03-counter-app/src/tests/base/09-promesas.test.js
@@ -1,21 +1,21 @@
 
-import { getHeroeByIdAsync } from "../../base/09-promesas";
+// import { getHeroeByIdAsync } from "../../base/09-promesas";
 
-describe('Pruebas de promesas', () => {
+// describe('Pruebas de promesas', () => {
 
-    // Hay que mandar el done en tareas asincronas!!!!!!
+//     // Hay que mandar el done en tareas asincronas!!!!!!
 
-    test('getHeroeByIdAsync debe retornar un heroe async', ( done ) => {
+//     test('getHeroeByIdAsync debe retornar un heroe async', ( done ) => {
 
-        const id = 1;
+//         const id = 1;
 
-        getHeroeByIdAsync( id )
-            .then( heroe => {
+//         getHeroeByIdAsync( id )
+//             .then( heroe => {
 
-                expect( heroe ).toBe( heroes[0] );
-                done();
-            });
-    });
+//                 expect( heroe ).toBe( heroes[0] );
+//                 done();
+//             });
+//     });
 
 
-})
+// })
