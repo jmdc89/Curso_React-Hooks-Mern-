e5ef8a1a2f16240581e923abe7539cab212b767f
Jose Manuel <jmdcarq89@gmail.com>
2020-11-28 23:04:55 +0100

    GifApp_AddCategory
    Creación del componente

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+
+export const GifExpertApp = () => {
+
+    
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    // const handleAdd = () => {
+    //     // setCategories( [ 'HunterXHunter', ...categories]);
+    //     setCategories( cats => [ ...cats, 'HunterXHunter']);
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory />
+            <hr />
+
+            
+
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }> { category } </li>
+                    })
+                }
+            </ol>
+        </>
+    );
+};
