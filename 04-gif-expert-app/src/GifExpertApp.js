b3bc26671ab76d64e3a89ffe74792734355cc20c
Jose Manuel <jmdcarq89@gmail.com>
2020-11-29 10:26:30 +0100

    Gif_FetchAppy
    Obtención de imagenes

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-
     
-    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
-
-    // const handleAdd = () => {
-    //     // setCategories( [ 'HunterXHunter', ...categories]);
-    //     setCategories( cats => [ ...cats, 'HunterXHunter']);
-    // }
+    const [categories, setCategories] = useState(['One Punch']);
 
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={ setCategories } />
             <hr />
-
-            
 
             <ol>
                 {
-                    categories.map( category => {
-                        return <li key={ category }> { category } </li>
-                    })
+                    categories.map( category  => (
+                        <GifGrid 
+                            key={ category }
+                            category={ category }
+                        />
+                    ))
                 }
             </ol>
+
         </>
-    );
-};
+    )
+}
--- 04-gif-expert-app/src/components/AddCategory.js
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
 
-    const [inputValue, setInputValue] = useState('Hola Mundo')
+    const [inputValue, setInputValue] = useState(''); // ''
 
     const handleInputChange = ( e ) => {
         setInputValue( e.target.value );
@@ -10,17 +11,26 @@ export const AddCategory = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Submit hecho')
+
+        if ( inputValue.trim().length > 2 ) {
+            setCategories( cats => [ inputValue, ...cats, ] );
+            setInputValue('');
+        }
+
     }
 
-
     return (
-        <form onSubmit={ handleSubmit}>   
-            <input
+        <form onSubmit={ handleSubmit }>
+            <input 
                 type="text"
                 value={ inputValue }
                 onChange={ handleInputChange }
             />
         </form>
-    );
-};
+    )
+}
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
