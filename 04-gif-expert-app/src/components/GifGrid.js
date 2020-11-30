d78309259007c5adcd9dea0ca64a78d2b1e1264c
Jose Manuel <jmdcarq89@gmail.com>
2020-11-30 09:02:13 +0100

    useFetchGifs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,43 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
+export const GifGrid = ({ category }) => {
 
-export const GifGrid = ( { category} ) => {
-
-    const [images, setImages] = useState([]);
-
-    useEffect( ( ) => {
-        getGifs();
-    }, [])
-
-
-
-    const getGifs = async() => {
-
-        const url = 'https:api.giphy.com/v1/gifs/search?q=Berserker&limit=10&api_key=rPw95VWQlAx2Npe45NJCoNzQzuq2QWM3';
-        const resp = await fetch( url );
-        const {data} = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages( gifs );
-
-    }
+    const { data:images, loading } = useFetchGifs( category );
 
     return (
         <>
-            <h3> { category } </h3>   
+            <h3 className="animate__animated animate__fadeIn"> { category } </h3>
+
+            { loading && <p className="animate__animated animate__flash">Loading</p> }
+
             <div className="card-grid">
+                
                 {
                     images.map( img => (
-                        <GifGridItem
+                        <GifGridItem 
                             key={ img.id }
                             { ...img }
                         />
@@ -47,4 +26,4 @@ export const GifGrid = ( { category} ) => {
             </div>
         </>
     )
-};
+}
