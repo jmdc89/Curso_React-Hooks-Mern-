f726b8a6f1d5337b89e369a6dfbff38f71f5e891
Jose Manuel <jmdcarq89@gmail.com>
2020-11-30 13:49:39 +0100

    CostumeHook_useEffect

---
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr />
+            
+        </>
+    )
+}
