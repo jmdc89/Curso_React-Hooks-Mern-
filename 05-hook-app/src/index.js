2320752389d551a217d8f9ec597b019de88c4738
Jose Manuel <jmdcarq89@gmail.com>
2020-12-01 21:51:34 +0100

    Tarea_Memorize

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -14,7 +15,7 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <CallbackHook />,
+    <Padre />,
   document.getElementById('root')
 );
 
