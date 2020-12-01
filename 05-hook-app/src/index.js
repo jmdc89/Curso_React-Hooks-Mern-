e6aefdb6fac9b064e5e698799016a9a4799b0ef8
Jose Manuel <jmdcarq89@gmail.com>
2020-12-01 11:22:49 +0100

    CustHook_useRef-RealCase

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <FocusScreen />,
+    <RealExampleRef />,
   document.getElementById('root')
 );
 
