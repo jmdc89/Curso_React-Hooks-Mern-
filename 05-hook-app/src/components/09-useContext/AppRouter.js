93b0afda243614622c70e8f1dc2a63d24d5af041
Jose Manuel <jmdcarq89@gmail.com>
2020-12-02 22:54:07 +0100

    Router_NavLink

--- 05-hook-app/src/components/09-useContext/AppRouter.js
+++ 05-hook-app/src/components/09-useContext/AppRouter.js
@@ -3,19 +3,28 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect,
   } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
+
+                <NavBar />
+
                 <Switch>
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
                     <Route exact path="/" component={HomeScreen} />
+
+                    {/* <Route component={ HomeScreen } /> */}
+                    <Redirect to="/" />
+
                 </Switch>
             </div>
         </Router>
