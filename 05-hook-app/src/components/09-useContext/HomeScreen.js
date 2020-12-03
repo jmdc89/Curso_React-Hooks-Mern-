f285cc895d7a99c8fa3ebb7835ddbcb134208c6f
Jose Manuel <jmdcarq89@gmail.com>
2020-12-03 09:23:49 +0100

    Create_UseContext

--- 05-hook-app/src/components/09-useContext/HomeScreen.js
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext, userContext } from './UserContext';
 
 export const HomeScreen = () => {
+
+    const userContext = useContext(UserContext);
+
+    console.log(userContext);
+
     return (
         <div>
             <h1>HomeScreen</h1>
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Jose',
+        email: 'jose@gmail.com'
+    }
+
     return (  
-        <AppRouter />      
+        <UserContext.Provider value={ user }>
+
+            <AppRouter />      
+
+        </UserContext.Provider>
     )
 }
