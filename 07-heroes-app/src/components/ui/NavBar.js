a2abb6d205772c4d5f57d97beb90ff27d98ae0d5
Jose Manuel <jmdcarq89@gmail.com>
2020-12-05 20:44:47 +0100

    HeroesApp_Logout

--- 07-heroes-app/src/components/ui/NavBar.js
+++ 07-heroes-app/src/components/ui/NavBar.js
@@ -1,10 +1,22 @@
 import React, { useContext } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useHistory } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const NavBar = () => {
 
-    const { user:{ name } } = useContext(AuthContext);
+    const { user:{ name }, dispatch} = useContext(AuthContext);
+    const history = useHistory();
+
+
+    const handleLogout = () => {
+
+        history.replace('/login');
+
+        dispatch({
+            type: types.logout
+        });
+    }
    
 
     return (
@@ -57,14 +69,12 @@ export const NavBar = () => {
                         { name }
                     </span>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={ handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
