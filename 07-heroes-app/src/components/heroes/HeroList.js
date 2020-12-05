850a291b6784367aa935840bb28f7ef709981458
Jose Manuel <jmdcarq89@gmail.com>
2020-12-05 10:58:14 +0100

    Cards_info

--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
-export const HeroList = ({publisher}) => {
+export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo(() => getHeroesByPublisher( publisher ), [ publisher ] );
 
-        return (
-            <ul>
-                {
-                    heroes.map( hero => (
-                        <li key={ hero.id }>
-                            { hero.superhero }
-                        </li>
-                    ))
-                }
-                
-            </ul>
-        )
+    return (
+        <div className="card-columns animate__animated animate__fadeIn">
+           {
+               heroes.map( hero => (
+                   <HeroCard 
+                        key={ hero.id }
+                        { ...hero }
+                    />
+                   
+               ))
+           } 
+        </div>
+    )
 }
