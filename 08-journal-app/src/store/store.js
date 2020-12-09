afa2ac6f4c9c8b2595d87f13fa240e4e7419db76
Jose Manuel <jmdcarq89@gmail.com>
2020-12-09 12:02:40 +0100

    uiReducer_store

--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -1,20 +1,10 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
+import { createStore, combineReducers } from 'redux';
 import { authReducer } from '../reducers/authReducer';
-import { uiReducer } from '../reducers/uiReducer';
 
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
-    auth: authReducer,
-    ui: uiReducer
-});
+    auth: authReducer
+})
 
 
-export const store = createStore(
-    reducers,
-    composeEnhancers(
-        applyMiddleware( thunk )
-    )
-);
+export const store = createStore(reducers);
