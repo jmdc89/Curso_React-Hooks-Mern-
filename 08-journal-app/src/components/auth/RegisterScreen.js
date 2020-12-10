af9b7e85c02497c494fb4e6efa9ac207b8691854
Jose Manuel <jmdcarq89@gmail.com>
2020-12-10 10:02:21 +0100

    HandleError_RegForm

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
+import validator from 'validator';
 
 export const RegisterScreen = () => {
 
@@ -15,16 +16,40 @@ export const RegisterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        console.log( name, email, password, password2 )
+
+        if ( isFormValid() ) {
+            console.log('Formulario correcto');
+        }  
     }
 
-
+    const isFormValid = () => {
+        
+        if ( name.trim().length === 0 ) {
+            // dispatch( setError('Name is required') )
+            console.log('Name is required');
+            return false;
+        } else if ( !validator.isEmail( email ) ) {
+            // dispatch( setError('Email is not valid') )
+            console.log('Email is not valid');
+            return false;
+        } else if ( password !== password2 || password.length < 5 ) {
+            // dispatch( setError('Password should be at least 6 characters and match each other') )
+            console.log('Password should be at least 6 characters and match each other');
+            return false
+        }
+        
+       return true;
+    }
 
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={ handleRegister }>
+
+                <div className= "auth_alert-error">
+                    Hola Mundo
+                </div>
 
                 <input 
                     type="text"
