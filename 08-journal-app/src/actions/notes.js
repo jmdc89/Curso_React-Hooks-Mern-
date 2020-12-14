6230b65ee6cb4c141fce0d6fcdc35402335d1af7
Jose Manuel <jmdcarq89@gmail.com>
2020-12-14 21:36:58 +0100

    StartUploading

--- 08-journal-app/src/actions/notes.js
+++ 08-journal-app/src/actions/notes.js
@@ -73,3 +73,27 @@ export const refreshNote = ( id, note ) => ({
         }
     }
 })
+
+export const startUploading = ( file ) => {
+    return async( dispatch, getState ) => {
+
+        const { active:activeNote } = getState().notes;
+
+        Swal.fire({
+            title: 'Uploading...',
+            text: 'Please wait...',
+            allowOutsideClick: false,
+            onBeforeOpen: () => {
+                Swal.showLoading();
+            }
+        });
+
+        const fileUrl = await fileUpload( file );
+        activeNote.url = fileUrl;
+
+        dispatch( startSaveNote( activeNote ) )
+        
+
+        Swal.close();
+    }
+}
--- 08-journal-app/src/components/notes/NotesAppBar.js
+++ 08-journal-app/src/components/notes/NotesAppBar.js
@@ -11,12 +11,35 @@ export const NotesAppBar = () => {
         dispatch( startSaveNote( active ) );
     }
 
+    const handlePictureClick = () => {
+        document.querySelector('#fileSelector').click();
+    }
+    
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if ( file ) {
+            dispatch( startUploading( file ) );
+        }
+    }
+
+
     return (
         <div className="notes__appbar">
             <span>09 de Diciembre 2020</span>
 
+            <input 
+                id="fileSelector"
+                type="file"
+                name="file"
+                style={{ display: 'none' }}
+                onChange={ handleFileChange }
+            />
+
             <div>
-                <button className="btn">
+                <button 
+                className="btn"
+                onClick={ handlePictureClick }
+                >
                     Picture
                 </button>
 
