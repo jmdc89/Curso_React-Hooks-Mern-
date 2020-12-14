2cb5ec9cc9a5656e4dfb5fe6495fcb07ec288a51
Jose Manuel <jmdcarq89@gmail.com>
2020-12-14 21:59:34 +0100

    NotesDelete

--- 08-journal-app/src/reducers/notesReducer.js
+++ 08-journal-app/src/reducers/notesReducer.js
@@ -54,12 +54,12 @@ export const notesReducer = ( state = initialState, action ) => {
                 )
             }
 
-        // case types.notesDelete:
-        //     return {
-        //         ...state,
-        //         active: null,
-        //         notes: state.notes.filter( note => note.id !== action.payload )
-        //     } 
+        case types.notesDelete:
+            return {
+                ...state,
+                active: null,
+                notes: state.notes.filter( note => note.id !== action.payload )
+            } 
 
         // case types.notesLogoutCleaning:
         //     return {
