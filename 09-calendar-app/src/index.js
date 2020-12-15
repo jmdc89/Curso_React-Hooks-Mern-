ce652cb811771a5d210305fa7187bb8098a86302
Jose Manuel <jmdcarq89@gmail.com>
2020-12-15 10:25:14 +0100

    npmrouterdom_init

--- 09-calendar-app/src/index.js
+++ 09-calendar-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CalendarApp } from './CalendarApp';
 
 
 ReactDOM.render(
-    <App />
+    <CalendarApp />
   document.getElementById('root')
 );
