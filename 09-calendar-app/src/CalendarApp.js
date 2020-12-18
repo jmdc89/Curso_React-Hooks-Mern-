63ffecc6bd93c34065cfa2aef4488170d2f97bed
Jose Manuel <jmdcarq89@gmail.com>
2020-12-18 11:36:33 +0100

    Actions_ui

--- 09-calendar-app/src/CalendarApp.js
+++ 09-calendar-app/src/CalendarApp.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
 import { AppRouter } from './router/AppRouter'
 
 export const CalendarApp = () => {
     return (
-        <div>
-            {/* <h1>Hola Mundo</h1> */}
-            <AppRouter />
-        </div>
+            <Provider store={ store }>
+                <AppRouter />
+            </Provider>
     )
 }
