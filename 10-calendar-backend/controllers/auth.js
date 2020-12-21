1d6b640f0734ea228009a8962d6bce9e437fea70
Jose Manuel <jmdcarq89@gmail.com>
2020-12-21 11:33:08 +0100

    express-validations

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,11 +1,21 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req, res = response ) => {
 
     const { name, email, password } = req.body;
 
-    res.json({
+    //manejo de errores
+    const errors = validationResult( req );
+    if ( !errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -17,6 +27,15 @@ const crearUsuario = (req, res = response ) => {
 const loginUsuario = (req, res = response ) => {
 
     const { email, password } = req.body;
+
+     //manejo de errores
+     const errors = validationResult( req );
+     if ( !errors.isEmpty()) {
+         return res.status(400).json({
+             ok: false,
+             errors: errors.mapped()
+         });
+     }
 
     res.json({
         ok: true,
