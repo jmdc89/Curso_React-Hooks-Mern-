615fadc6ff2834807d8f03dc75fd297a08f195d9
Jose Manuel <jmdcarq89@gmail.com>
2020-12-22 12:43:46 +0100

    index_update

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
@@ -8,17 +9,18 @@ const app = express();
 // Base de datos
 dbConnection();
 
-//Directorio público
-app.use( express.static('public'));
+// CORS
+app.use(cors())
 
-//Lectura y parseo del body
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
 app.use( express.json() );
-
 
 // Rutas
 app.use('/api/auth', require('./routes/auth') );
-//TODO: auth // crear, login, renew
-//TODO: CRUD: Eventos
+// TODO: CRUD: Eventos
 
 
 
@@ -26,3 +28,4 @@ app.use('/api/auth', require('./routes/auth') );
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
+
