8106a6f6690b2a36c747a1ff9304f0bdac8b479d
Jose Manuel <jmdcarq89@gmail.com>
2020-12-23 12:48:14 +0100

    crearEvento

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -11,3 +11,28 @@ const getEventos = async( req, res = response ) => {
         eventos
     });
 }
+
+const crearEvento = async ( req, res = response ) => {
+
+    const evento = new Evento( req.body );
+
+    try {
+
+        evento.user = req.uid;
+        
+        const eventoGuardado = await evento.save();
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+}
