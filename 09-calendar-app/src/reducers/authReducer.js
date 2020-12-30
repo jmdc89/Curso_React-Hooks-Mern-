f6289f80065bbe6b89f508fa18798b1aeff43cff
Jose Manuel <jmdcarq89@gmail.com>
2020-12-30 08:47:14 +0100

    authReducer_09

---
+++ 09-calendar-app/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    checking: true,
+    // uid: null,
+    // name: null
+}
+
+export const authReducer = ( state = initialState, action ) => {
+
+    switch ( action.type ) {
+        default:
+            return state;
+    }
+}
