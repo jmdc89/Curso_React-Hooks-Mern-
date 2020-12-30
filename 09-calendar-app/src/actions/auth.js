8e374b722b351a77787971001a278050ea6728db
Jose Manuel <jmdcarq89@gmail.com>
2020-12-30 09:46:47 +0100

    Logout-checking

--- 09-calendar-app/src/actions/auth.js
+++ 09-calendar-app/src/actions/auth.js
@@ -67,3 +67,23 @@ export const startChecking = () => {
         }
     }
 }
+
+const checkingFinish = () => ({ type: types.authCheckingFinish });
+
+
+
+const login = ( user ) => ({
+    type: types.authLogin,
+    payload: user
+});
+
+
+export const startLogout = () => {
+    return ( dispatch ) => {
+
+        localStorage.clear();
+        dispatch( logout() );
+    }
+}
+
+const logout = () => ({ type: types.authLogout })
