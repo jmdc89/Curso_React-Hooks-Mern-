b600f67b1449307f5bc427f6e5ad9e5aab20bbe1
Jose Manuel <jmdcarq89@gmail.com>
2020-12-30 09:45:41 +0100

    Update NavBar

--- 09-calendar-app/src/components/ui/NavBar.js
+++ 09-calendar-app/src/components/ui/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const NavBar = () => {
+
+    const { name } = useSelector( state => state.auth );
+
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">
-                Pedro
+                {name}
             </span>
 
             <button className="btn btn-outline-danger">
