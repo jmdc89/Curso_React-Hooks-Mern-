c34b9362691654965594044772da15870f212b6c
Jose Manuel <jmdcarq89@gmail.com>
2020-12-30 09:13:10 +0100

    auth_startlogin

---
+++ 09-calendar-app/src/actions/auth.js
@@ -0,0 +1,27 @@
+import { fetchSinToken, fetchConToken } from '../helpers/fetch';
+import { types } from '../types/types';
+import Swal from 'sweetalert2';
+
+
+
+export const startLogin = ( email, password ) => {
+    return async( dispatch ) => {
+
+        const resp = await fetchSinToken( 'auth', { email, password }, 'POST' );
+        const body = await resp.json();
+
+        if( body.ok ) {
+            localStorage.setItem('token', body.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+            }) )
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+        }
+        
+
+    }
+}
