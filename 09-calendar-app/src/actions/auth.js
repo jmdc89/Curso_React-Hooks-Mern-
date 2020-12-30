ed843cb637366d5e4d30b6398bd342f58487f98b
Jose Manuel <jmdcarq89@gmail.com>
2020-12-30 09:24:57 +0100

    auth_startRegister

--- 09-calendar-app/src/actions/auth.js
+++ 09-calendar-app/src/actions/auth.js
@@ -26,3 +26,24 @@ export const startLogin = ( email, password ) => {
     }
 }
 
+export const startRegister = ( email, password, name ) => {
+    return async( dispatch ) => {
+
+        const resp = await fetchSinToken( 'auth/new', { email, password, name }, 'POST' );
+        const body = await resp.json();
+
+        if( body.ok ) {
+            localStorage.setItem('token', body.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+            }) )
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+        }
+
+
+    }
+}
